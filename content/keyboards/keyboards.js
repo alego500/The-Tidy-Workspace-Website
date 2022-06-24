8294c876aec1233a3c304c5c990220970ac71fbd
alego500 <alego500@gmail.com>
2022-06-24 23:12:14 +0200

    initial commit

---
+++ content/keyboards/keyboards.js
@@ -0,0 +1,11 @@
+var sitedata_keyboards = {
+    "Danish": {
+        mac: "danish_mac.html",
+    },
+    "English US": {
+        mac: "english-us_mac.html",
+    },
+    "English UK": {
+        mac: "english-uk_mac.html",
+    }
+};
